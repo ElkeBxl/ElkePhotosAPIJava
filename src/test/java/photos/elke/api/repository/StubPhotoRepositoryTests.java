f3597133fd3f1d283ae87e717dbe37bf12e96e0f
Elke Heymans <elke@Elkes-MBP-2.lan>
2018-01-29 22:03:42 +0100

    Wrote first JUnit tests for the StubPhotoRepository

--- src/test/java/photos/elke/api/repository/StubPhotoRepositoryTests.java
+++ src/test/java/photos/elke/api/repository/StubPhotoRepositoryTests.java
@@ -1,4 +1,46 @@
 package photos.elke.api.repository;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import photos.elke.api.model.Photo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
 public class StubPhotoRepositoryTests {
+
+    private StubPhotoRepository repository;
+
+    public StubPhotoRepositoryTests() {
+        this.repository = new StubPhotoRepositoryImpl();
+    }
+
+    @Test
+    public void contexLoads() throws Exception {
+        assertThat(this.repository).isNotNull();
+    }
+
+    @Test
+    public void canAddNewPhoto() throws Exception {
+        long currentCount = this.repository.count();
+        this.repository.save(new Photo(4));
+        assertThat(this.repository.count()).isEqualTo(currentCount + 1);
+    }
+
+    @Test
+    public void canAddMultiplePhotos() throws Exception {
+        long currentCount = this.repository.count();
+        List<Photo> newPhotos = new ArrayList<Photo>();
+        newPhotos.add(new Photo(5));
+        newPhotos.add(new Photo(6));
+        newPhotos.add(new Photo(7));
+        this.repository.saveAll(newPhotos);
+        assertThat(this.repository.count()).isEqualTo(currentCount + 3);
+    }
 }
